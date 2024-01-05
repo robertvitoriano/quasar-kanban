6cfefb5b091c65c5e294f13f1bc166ce00756192
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-01-05 09:54:45 +0900

    fix api key

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -6,7 +6,7 @@ import axios from "axios";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: "http://54.91.129.254:8000/api" });
+const api = axios.create({ baseURL: "http://127.0.0.1:8000/api" });
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token")
