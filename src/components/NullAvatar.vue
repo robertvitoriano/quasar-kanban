a63289f2717239b0ddbc90d7b5e3ecd08f8a135f
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-06-25 17:47:28 +0900

    fix avatar height

--- src/components/NullAvatar.vue
+++ src/components/NullAvatar.vue
@@ -15,8 +15,8 @@ const props = defineProps(["title"]);
   justify-content: center;
   align-items: center;
   font-size: inherit;
-  width: 100%;
-  height: 100%;
+  min-width: 40px;
+  min-height: 40px;
   border-radius: 50%;
 }
 </style>
